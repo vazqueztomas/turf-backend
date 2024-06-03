f7c7e940ea90983c30a9d312eeeb671f5d26caac
Tomas Vazquez <vazquezt2018@gmail.com>
2024-06-03 18:46:17 -0300

    using classes in DatabaseConnection

--- database.py
+++ database.py
@@ -1,5 +1,15 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+class DatabaseConnection:
+    def __init__(self,uri,database_name):
+        self.client = AsyncIOMotorClient(uri)
+        self.database_name = self.client[database_name]
 
-client = AsyncIOMotorClient("mongodb+srv://vazquezt2018:turf1234@cluster0.mmlsepe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db = client.turf
-users_collection = db.users
+    def get_collection(self, collection_name):
+        return self.db[collection_name]
+
+db_uri = 'mongodb+srv://vazquezt2018:turf1234@cluster0.mmlsepe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+db_name = 'turf'
+
+database = DatabaseConnection(db_uri, db_name)
+
+users_collection = database.get_collection('users')
