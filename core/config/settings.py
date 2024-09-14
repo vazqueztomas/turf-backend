4105b0ffe2ddbe893253f1fdc3543b840de714b3
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-09-14 17:49:53 -0300

    feat: add global settings using pydantic

---
+++ core/config/settings.py
@@ -0,0 +1,15 @@
+
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+
+class Settings(BaseSettings):
+    DB_NAME: str = Field(alias='DATABASE_NAME', default=None)
+    DB_USER: str = Field(alias='DATABASE_USER', default=None)
+    DB_PASSWORD: str = Field(alias='DATABASE_PASSWORD', default=None)
+    DB_HOST: str = Field(alias='DATABASE_HOST', default=None)
+    DB_PORT: int = Field(alias='DATABASE_PORT', default=None)
+    DB_URI: str = Field(alias='DATABASE_URL', default=None)
+
+
+settings = Settings()
