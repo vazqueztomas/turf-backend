bd7e802978009b6d6b40040277b3ece46b634d9d
Tomas Vazquez <vazquezt2018@gmail.com>
2024-11-06 00:58:18 -0300

    fix(config): fix cors problems

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",  # Agregar este origen adicional
+    "https://turf-frontend-two.vercel.app",
 ]
 
 app.add_middleware(
