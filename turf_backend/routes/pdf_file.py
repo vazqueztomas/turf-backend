fcfb036a1aa70955a4558b8a59bb1f7cdfb3d3f9
Tomas Vazquez <vazquezt2018@gmail.com>
2024-11-29 16:58:46 -0300

    fix(routes): correct type hint for retrieve_file function to use comma for response model

--- turf_backend/routes/pdf_file.py
+++ turf_backend/routes/pdf_file.py
@@ -30,7 +30,7 @@ def list_available_files(location: AvailableLocations) -> list[str]:
 @router.get("/{location}/{filename}", response_model=None)
 def retrieve_file(
     location: AvailableLocations, filename: str
-) -> Union[JSONResponse | FileResponse]:
+) -> Union[JSONResponse, FileResponse]:
     pdf_file_controller = PdfFileController()
 
     file_location = pdf_file_controller.retrieve_file(location, filename)
