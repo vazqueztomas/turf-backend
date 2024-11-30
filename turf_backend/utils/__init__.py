fb88a53507e498b770a5156e5d700c0e4ba71cf2
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-11-30 12:38:34 -0300

    feat(config): add loguru for enhanced logging functionality

--- turf_backend/utils/__init__.py
+++ turf_backend/utils/__init__.py
@@ -1,4 +1,5 @@
 from .date import extract_date_from_pdf
 from .http_requests import HTTPMethods, request_http
+from .logger import log
 
-__all__ = ["HTTPMethods", "extract_date_from_pdf", "request_http"]
+__all__ = ["HTTPMethods", "extract_date_from_pdf", "log", "request_http"]
--- turf_backend/utils/http_requests.py
+++ turf_backend/utils/http_requests.py
@@ -3,6 +3,8 @@ from typing import Optional, TypedDict
 
 import requests
 
+from .logger import LogLevels, log
+
 
 class HTTPMethods(str, Enum):
     GET = "GET"
@@ -21,9 +23,13 @@ def request_http(
 ) -> HTTPResponse:
     try:
         response = requests.request(method.value, url)  # type: ignore[attr-defined]
+        log(f"HTTP {method} request to {url} returned {response.status_code}")
         return {"status_code": response.status_code, "text": response.text}  # noqa: TRY300
     except requests.exceptions.RequestException as error:
-        print(f"Error while making HTTP {method} request to {url}: {error=}")  # noqa: T201
+        log(
+            f"Error while making HTTP {method} request to {url}: {error=}",
+            LogLevels.ERROR,
+        )
         return {
             "status_code": 500,
             "text": f"Error while making HTTP {method} request to {url}: {error=}",
